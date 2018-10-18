2849397b1ee76c21b419c79a4b97bae2e6b60603
Twoflow3r <klimov.alex.88@mail.ru>
2018-10-18 20:57:31 +0300

    togggle full

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,133 @@
 (function() {
 var card = document.getElementsByClassName('card');
 var circle = document.getElementsByClassName('circle');
-var content = document.getElementsByClassName('content-card');
+var content = document.getElementsByClassName('card-content');
 var link = document.getElementsByClassName('link');
 console.log(card,circle,content);
 
-
-function linkClick(e) {
-    var element = e.target.parentNode;
-    if ( element.classList == "card-wrapper") {
-    element.classList.remove('card-wrapper');
-element.classList.add('card-wrapper-select');
-    }
-}
-
-
-for (i = 0; i < link.length; i++) {
-    link[i].addEventListener('click', function(e) {
-        linkClick(e);
-  },false);
-}
-
-
-
+(function() {
+    var card = document.getElementsByClassName('card');
+    var circle = document.getElementsByClassName('circle');
+    var content = document.getElementsByClassName('content-card');
+    var link = document.getElementsByClassName('link');
+    console.log(card,circle,content);
     
-    function cardClick(e) {
-        var element = e.target.preventDefault;
+    
+    function linkClick(e) {
+        var element = e.target.parentNode;
         if ( element.classList == "card-wrapper") {
         element.classList.remove('card-wrapper');
     element.classList.add('card-wrapper-select');
-    } else if ( element.classList == "card-wrapper-select card-wrapper-select-hover" ) {
-        element.classList.remove('card-wrapper-select');
-        element.classList.remove('card-wrapper-select-hover');
-        element.classList.add('card-wrapper');
+        }
     }
+    
+    
+    for (i = 0; i < link.length; i++) {
+        link[i].addEventListener('click', function(e) {
+            linkClick(e);
+      },false);
     }
+    
+    
+    
+        
+        function cardClick(e) {
+            var element = e.target.preventDefault;
+            if ( element.classList == "card-wrapper") {
+            element.classList.remove('card-wrapper');
+        element.classList.add('card-wrapper-select');
+        } else if ( element.classList == "card-wrapper-select card-wrapper-select-hover" ) {
+            element.classList.remove('card-wrapper-select');
+            element.classList.remove('card-wrapper-select-hover');
+            element.classList.add('card-wrapper');
+        }
+        }
+    
+    
+        for (i = 0; i < card.length; i++) {
+          card[i].addEventListener('click', function(e) {
+              cardClick(e);
+          },false);
+        }
+    
+        function cardClickCircle(e) {
+            var element = e.target.parentNode.parentNode.parentNode;
+            if ( element.classList == "card-wrapper") {
+            element.classList.remove('card-wrapper');
+        element.classList.add('card-wrapper-select');
+        } else if ( element.classList == "card-wrapper-select" ) {
+            element.classList.remove('card-wrapper-select');
+        element.classList.add('card-wrapper');    
+        } else if ( element.classList == "card-wrapper-select card-wrapper-select-hover" ) {
+            element.classList.remove('card-wrapper-select');
+            element.classList.remove('card-wrapper-select-hover');
+            element.classList.add('card-wrapper');
+        }
+        }
+    
+
+    
+        for (i = 0; i < circle.length; i++) {
+            circle[i].addEventListener('click', function(e) {
+                cardClickCircle(e);
+            },false);
+          }
+              
+          
+    
+          function cardClickContent(e) {
+            var element = e.target.parentNode.parentNode.parentNode;
+            if ( element.classList == "card-wrapper") {
+            element.classList.remove('card-wrapper');
+        element.classList.add('card-wrapper-select');
+        } else if ( element.classList == "card-wrapper-select" ) {
+            element.classList.remove('card-wrapper-select');
+        element.classList.add('card-wrapper');    
+         } else if ( element.classList == "card-wrapper-select card-wrapper-select-hover" ) {
+            element.classList.remove('card-wrapper-select');
+            element.classList.remove('card-wrapper-select-hover');
+            element.classList.add('card-wrapper');
+        }
+        }
+    
+    
+    
+        for (i = 0; i < content.length; i++) {
+            content[i].addEventListener('click', function(e) {
+                cardClickContent(e);
+            },false);
+          }
+    
+    
+          function leaveMouse (e) {
+            var element = e.target.parentNode;
+            if (element.classList == 'card-wrapper-select'){
+            element.classList.add('card-wrapper-select-hover');
+            }
+        };
+    
+        for (i = 0; i < content.length; i++) {
+            card[i].addEventListener('mouseleave',function(e) {
+                leaveMouse(e);
+            },false);
+        }
+    })();
+
+    
+    function cardClick(e) {
+        var element = e.target.parentNode;
+        if ( element.classList == "card-wrapper") {
+            element.classList.remove('card-wrapper');
+        element.classList.add('card-wrapper-select');
+        } else if ( element.classList == "card-wrapper-select" ) {
+            element.classList.remove('card-wrapper-select');
+        element.classList.add('card-wrapper');    
+        } else if ( element.classList == "card-wrapper-select card-wrapper-select-hover" ) {
+            element.classList.remove('card-wrapper-select');
+            element.classList.remove('card-wrapper-select-hover');
+            element.classList.add('card-wrapper');
+        }
+        }
 
 
     for (i = 0; i < card.length; i++) {
